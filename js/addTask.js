e7117a0e808ebc5e8a465db5efb9982837966c2e
Jan Steindorf <funstyle@hotmail.de>
2022-02-09 18:05:47 +0100

    started "Task.html"

--- js/addTask.js
+++ js/addTask.js
@@ -1,3 +1,16 @@
-function addtask(){
-    
+function addtask() {
+
+}
+
+let isMenu = false;
+
+
+function addMember() {
+    if (isMenu == false) {
+        document.getElementById('assign-menu').classList.remove('d-none');
+        isMenu = true;
+    }else{
+        document.getElementById('assign-menu').classList.add('d-none');
+        isMenu = false;
+    }
 }
