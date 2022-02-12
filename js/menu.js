1c54e58ade08199f37acaa46123bd98283b64e1e
Aldin <aldin.bijedic@gmx.at>
2022-02-12 22:42:28 +0100

    created menu.js

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+function burgerMenuToggle() {
+    document.getElementById('menu-mobile').classList.toggle('active');
+    document.getElementById('menu-toggle').classList.toggle('active');
+    document.querySelector('.only-mobile').classList.toggle('active');
+}
